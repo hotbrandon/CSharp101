961762d70075297182b3c670234d9fee203b0f0d
hotbrandon <hotbrandon@hotmail.com>
2021-04-08 10:49:21 +0800

    Add project files.

---
+++ CSharpIntermediate/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpIntermediate
+{
+    class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // static constructor does not take access modifiers or any parameters
+        static Employee()
+        {
+            Console.WriteLine("the static constructor gets called only once, no matter how many objects are instantiated");
+        }
+        // default constructor
+        public Employee()
+        {
+            Console.WriteLine("The default constructor gets called upon object instantiation");
+        }
+    }
+}
---
+++ CSharpIntermediate/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CSharpIntermediate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var emp1 = new Employee();
+            var emp2 = new Employee();
+
+            Console.WriteLine("in Main method of Program!");
+        }
+    }
+}
